5420360ac8e0af3d0c37449c89149607c2651c39
DESKTOP-H6FUKM1\deepak <er.deepakporwal@gmail.com>
2021-07-24 23:25:14 +0100

    added swagger and values controller

---
+++ SampleCoreApi/clsResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleCoreApi
+{
+    public class clsResponse
+    {
+        public int responsecode { get; set; }
+        public string message { get; set; }
+    }
+}
